99522efe2feee67fafd352642f562df7bc8c4f5a
hugo charon <charon.hugo@gmail.com>
2021-02-09 16:22:15 +0100

    login form / register form / sending mail to verify the new user done close #1 close #2

---
+++ src/Form/LoginFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email',EmailType::class)
+            ->add('password',PasswordType::class)
+            ->add('_remember_me',CheckboxType::class, [
+                'required' => false
+            ])
+        ;
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_token_id' => 'authenticate'
+        ]);
+    }
+}
