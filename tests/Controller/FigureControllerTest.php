00a8a63fc308b4c462e8db834e260db0ddd32c1a
hugo charon <charon.hugo@gmail.com>
2021-09-08 15:31:00 +0200

    feat (testing all pages to the project connected and Noconnected ) #38

--- tests/Controller/FigureControllerTest.php
+++ tests/Controller/FigureControllerTest.php
@@ -12,12 +12,12 @@ class FigureControllerTest extends WebTestCase
     /**
      * @dataProvider urlProvider
      */
-    public function testPageIsSuccessful($url, $expectedStatus = 200)
+    public function testPageIsSuccessfulWhenConnected($url, $expectedStatus)
     {
 
         $client = self::createClient();
         $userRepository = $client->getContainer()->get(UserRepository::class);
-        $client->loginUser($userRepository->find(1));
+        $client->loginUser($userRepository->findOneBy(['userName' => 'user0']));
 
         $client->request('GET', $url);
         $this->assertResponseStatusCodeSame($expectedStatus);
@@ -25,25 +25,57 @@ class FigureControllerTest extends WebTestCase
 
     public function urlProvider()
     {
-        yield "homepage" => ['/'];
-
-        yield "figure create" => ['/add/figure'];
-        yield "figure show" => ['/show/figure/1/ma-figure-n01'];
+        yield "homepage" => ['/', 200];
+        yield "figure create" => ['/add/figure',200];
+        yield "figure show" => ['/show/figure/1/ma-figure-n01',200];
         yield "figure delete" => ['/deleted/figure/1', 302];
-        yield "figure edit" => ['/edit/figure/1'];
-//        yield "comment edit" => ['/edit/comment/1',302];
-//        yield "1" => ['/deleted/comment/1'];
-        yield "2" => ['/edit/image/1'];
-        yield "3" => ['/delete/image/1',302];
-//        yield "3" => ['/verify/email/{token}'];
-        yield "" => ['/login',302];
-//        yield "" => ['/reset_your_password'];
-//        yield "" => ['/new_password/auth/{token}'];
-//        yield "" => [''];
-//        yield "" => ['/register'];
-//        yield "" => ['/register'];
-//        yield "" => ['/register'];
-//        yield "" => ['/register'];
+        yield "figure edit" => ['/edit/figure/1',200];
+        yield "comment edit" => ['/edit/comment/1',200];
+        yield "login" => ['/login', 302];
+        yield "comment delete" => ['/deleted/comment/1', 302];
+        yield "image edit" => ['/edit/image/1',200];
+        yield "image delete" => ['/delete/image/1', 302];
+        yield "mail de verification" => ['/verify/email/{token}', 404];
+        yield "reset password" => ['/reset_your_password',200];
+        yield "new password" => ['/new_password/auth/{token}',200];
+
     }
+
+    /**
+     * @dataProvider urlProviderTest
+     */
+    public function testPageIsSuccessfulWhenNoConnected($url, $expectedStatus, $expectedRedirect = null)
+    {
+
+        $client = self::createClient();
+
+
+        $client->request('GET', $url);
+
+        $this->assertResponseStatusCodeSame($expectedStatus);
+
+        if($expectedRedirect){
+            $this->assertResponseRedirects($expectedRedirect);
+        }
+    }
+
+    public function urlProviderTest()
+    {
+        yield "homepage" => ['/' ,200];
+        yield "figure create" => ['/add/figure',302, "/login"];
+        yield "figure show" => ['/show/figure/1/ma-figure-n01' ,200];
+        yield "figure delete" => ['/deleted/figure/1' ,302, "/login"];
+        yield "figure edit" => ['/edit/figure/1',302, "/login"];
+        yield "comment edit" => ['/edit/comment/1',302, "/login"];
+        yield "login" => ['/login',200];
+        yield "comment delete" => ['/deleted/comment/1',302, "/login"];
+        yield "image edit" => ['/edit/image/1',302, "/login"];
+        yield "image delete" => ['/delete/image/1',302, "/login"];
+        yield "mail de verification" => ['/verify/email/{token}',404];
+        yield "reset password" => ['/reset_your_password',200];
+        yield "new password" => ['/new_password/auth/{token}',200];
+
+    }
+
 
 }
